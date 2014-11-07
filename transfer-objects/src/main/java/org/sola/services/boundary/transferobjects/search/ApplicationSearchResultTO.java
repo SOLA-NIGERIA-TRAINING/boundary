fbb2921fa0f96e59fa8ea1e2368399ce6fd487d0
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-11-07 17:48:03 +0100

    starting point

--- transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/search/ApplicationSearchResultTO.java
+++ transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/search/ApplicationSearchResultTO.java
@@ -1,28 +1,30 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 package org.sola.services.boundary.transferobjects.search;
@@ -30,18 +32,17 @@ package org.sola.services.boundary.transferobjects.search;
 import org.sola.services.boundary.transferobjects.casemanagement.ApplicationSummaryTO;
 
 public class ApplicationSearchResultTO extends ApplicationSummaryTO {
-    
+
     private String status;
     private String assigneeId;
     private String assigneeName;
     private String contactPerson;
     private String agent;
-    private String serviceList; 
+    private String serviceList;
     private String serviceStatusList;
     private String parcel;
-    private String section;
     
-    public ApplicationSearchResultTO(){
+    public ApplicationSearchResultTO() {
         super();
     }
 
@@ -53,14 +54,6 @@ public class ApplicationSearchResultTO extends ApplicationSummaryTO {
         this.parcel = parcel;
     }
 
-    public String getSection() {
-        return section;
-    }
-
-    public void setSection(String section) {
-        this.section = section;
-    }
-    
     public String getAgent() {
         return agent;
     }
@@ -68,7 +61,7 @@ public class ApplicationSearchResultTO extends ApplicationSummaryTO {
     public void setAgent(String agent) {
         this.agent = agent;
     }
-    
+
     public String getStatus() {
         return status;
     }
@@ -76,7 +69,7 @@ public class ApplicationSearchResultTO extends ApplicationSummaryTO {
     public void setStatus(String status) {
         this.status = status;
     }
-    
+
     public String getAssigneeName() {
         return assigneeName;
     }
@@ -92,7 +85,7 @@ public class ApplicationSearchResultTO extends ApplicationSummaryTO {
     public void setAssigneeId(String assigneeId) {
         this.assigneeId = assigneeId;
     }
-    
+
     public String getContactPerson() {
         return contactPerson;
     }
@@ -100,7 +93,7 @@ public class ApplicationSearchResultTO extends ApplicationSummaryTO {
     public void setContactPerson(String contactPerson) {
         this.contactPerson = contactPerson;
     }
-    
+
     public String getServiceList() {
         return serviceList;
     }
@@ -116,6 +109,4 @@ public class ApplicationSearchResultTO extends ApplicationSummaryTO {
     public void setServiceStatusList(String serviceStatusList) {
         this.serviceStatusList = serviceStatusList;
     }
-    
-    
 }
